bf747223de72185833f7492bfd73df0ff7fc6bff
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2025-09-07 23:35:52 +0900

    feat(catalog)

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+	return render(request, 'home.html')
+
+def contacts(request):
+	if request.method == "POST":
+		name = request.POST.get("name")
+		phone = request.POST.get("phone")
+		message = request.POST.get("message")
+		return HttpResponse(f"Срасибо {name}! Данные получены!!")
+	return render(request, 'contacts.html')
