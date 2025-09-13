5c23829c6993848f380dae21cc4892b9a048fdd0
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2025-09-13 17:27:28 +0900

    feat: подключена PostgreSQL, добавлены модели Category и Product, миграции, фикстуры, админка и команда для тестовых данных )

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = [
+            "name",
+        ]
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        verbose_name="Фото", help_text="Добавьте фото продукта", blank=True, null=True
+    )
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        verbose_name="Категория товара",
+        help_text="Введите категорию товара",
+        blank=True,
+        null=True,
+        related_name="categories",
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена",
+        help_text="Введите цену товара",
+    )
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата добавления")
+    updated_at = models.DateField(auto_now_add=True, verbose_name="Дата изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category"]
+
+    def __str__(self):
+        return f"{self.name}"
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-	return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def contacts(request):
-	if request.method == "POST":
-		name = request.POST.get("name")
-		phone = request.POST.get("phone")
-		message = request.POST.get("message")
-		return HttpResponse(f"Срасибо {name}! Данные получены!!")
-	return render(request, 'contacts.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(f"Спасибо {name}! Данные получены!!")
+    return render(request, "contacts.html")
